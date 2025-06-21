038bd65b3e1dca60422f18d9e00ac5cc7bd35712
nazekkosh <nazek.koshaji549@comunidadunir.net>
2025-06-21 15:05:12 +0200

    fix: export server function for Vercel

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use("/api", router);
+module.exports = (req, res) => {
+    const server = jsonServer.create();
+    const router = jsonServer.router("db.json");
+    const middlewares = jsonServer.defaults();
 
-module.exports = server;
+    server.use(middlewares);
+    server.use(jsonServer.bodyParser);
+    server.use("/api", router);
+
+    return server(req, res);
+};
