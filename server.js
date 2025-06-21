36ed832f2b0077af89767e87ee9d1a2660f0063e
nazekkosh <nazek.koshaji549@comunidadunir.net>
2025-06-21 15:00:11 +0200

    Fix Vercel deployment: json-server setup

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use("/api", router); // tu API estará en /api/reservas
+server.use("/api", router);
 
-server.listen(3000, () => {
-    console.log("JSON Server is running");
-});
+module.exports = server;
